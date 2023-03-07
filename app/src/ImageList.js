77bbaebb888ff3beb76d6fdaa0d37711b645ab02
Gorm2303 <gormdk333@gmail.com>
2023-03-07 18:15:30 +0100

    Fixed the Issue on ImageList and made pagination for the catalog

--- app/src/ImageList.js
+++ app/src/ImageList.js
@@ -4,14 +4,15 @@ function ImageList({ httpResponse }) {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    setImages(httpResponse.results);
+    console.log(httpResponse)
+    setImages(httpResponse);
     
   }, [httpResponse]);
 
   return (
     <div>
       {images.map((image, index) => (
-        <img width={200} src={'https://image.tmdb.org/t/p/original' + image.poster_path} alt={`${index + 1}`} />
+        <img key={image.poster_path} width={200} src={'https://image.tmdb.org/t/p/original' + image.poster_path} alt={`${index + 1}`} />
       ))}
     </div>
   );
--- app/src/VideoList.js
+++ app/src/VideoList.js
@@ -4,23 +4,40 @@ import ImageList from './ImageList';
 
 export default function VideoList(props) {
     const [httpResponse, setHttpResponse] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await axios.get('https://api.themoviedb.org/3/movie/upcoming?api_key=0c2928a0d441bf2ce59630be504699c4&language=en-US&page=1')//'/api/v1/videometadata');
-        setHttpResponse(response.data);
-      } catch (error) {
-        console.error(error);
-      }
+    try {
+      fetchData();
+    } catch (error) {
+      console.log("Could not GET response")
+      console.error(error);
     }
+  }, [currentPage]);
 
-    fetchData();
-  }, []);
+  async function fetchData() {
+    const response = await axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=0c2928a0d441bf2ce59630be504699c4&language=en-US&page=${currentPage}`)//'/api/v1/videometadata');
+    console.log(response);
+    console.log(response.data);
+    console.log(response.data.results);
+    setHttpResponse(response.data.results);
+}
+
+function handleNextPage() {
+  setCurrentPage(currentPage + 1);
+}
+
+function handlePrevPage() {
+  if (currentPage > 1) {
+    setCurrentPage(currentPage - 1);
+  }
+}
 
   return (
     <div>
       <ImageList httpResponse={httpResponse} />
+      <button onClick={handlePrevPage}>Previous</button>
+      <button onClick={handleNextPage}>Next</button>
     </div>
   );
 }
