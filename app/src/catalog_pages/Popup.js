bb0210238800bfaf58a5c87bccd9bbe79e83eccc
Mikkel Plagborg <mplagborg@hotmail.com>
2023-03-10 13:50:50 +0100

    Added videoplayer

--- app/src/catalog_pages/Popup.js
+++ app/src/catalog_pages/Popup.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import './Popup.css'
 
 function Popup({active, metadata, onClose}) {
@@ -20,7 +21,9 @@ function Popup({active, metadata, onClose}) {
         <div className="popup-content">
           <span className="close" onClick={handleClose}>&times;</span>
           <h1>{metadata.title}</h1>
-          <img width={400} src={'https://image.tmdb.org/t/p/original' + metadata.poster_path} alt={""}/>
+          <Link to={{ pathname: '/player', search: `?url=${metadata.video_path}` }}>
+            <img width={400} src={'https://image.tmdb.org/t/p/original' + metadata.poster_path} alt={""}/>
+          </Link>
           <div>
             <table>
               <tbody>
--- app/src/player_pages/PlayerPage.js
+++ app/src/player_pages/PlayerPage.js
@@ -1,11 +1,15 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import VideoPlayer from './VideoPlayer';
 
-export default function App() {
-    const videoUrl = 'https://www.youtube.com/watch?v=d46Azg3Pm4c'
+export default function PlayerPage() {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+  const videoUrl = searchParams.get('url');
 
-    return (
-        <div>
-            <VideoPlayer url={videoUrl} />
-        </div>
-    )
+  return (
+    <div>
+      <VideoPlayer url={videoUrl} />
+    </div>
+  )
 }
