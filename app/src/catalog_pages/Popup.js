3d57a77f78a9cbdeed2ab59ee4cbbaa4c2038da3
Gorm2303 <gormdk333@gmail.com>
2023-04-26 14:26:41 +0200

    Using bootstrap instead of own css

--- app/src/catalog_pages/Popup.js
+++ app/src/catalog_pages/Popup.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import './Popup.css'
+import { Modal, Button, Table, Badge } from 'react-bootstrap';
 
 
 function Popup({active, metadata, onClose}) {
@@ -15,39 +15,52 @@ function Popup({active, metadata, onClose}) {
     onClose();
   }
 
-  return (
-    showPopup &&
-    <span>
-      <div className="popup">
-        <div className="popup-content">
-          <span className="close" onClick={handleClose}>&times;</span>
-          <h1>{metadata.title}</h1>
-          <Link to={{ pathname: '/player', search: `?url=${metadata.video}` }}>
-            <img width={400} src={metadata.poster} alt={""}/>
-          </Link>
-          <div>
-            <table>
-              <tbody>
-                <tr className='smallInfo'>
-                  <td className='singleText'><b>Release: </b>{metadata.date}</td>
-                  <td className='genre'><b>Genre: </b>
-                    {metadata.genre.map((genre, index) => (
-                      <span key={index}>{genre}</span>
-                    ))}
-                  </td>
-                  <td className='description'><b>Description: </b>{metadata.description}</td>
-                </tr>
-                <tr className='smallInfo'>
-                  <td className='singleText'><b>Length: </b>??</td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-        </div>
+return (
+  showPopup &&
+  <Modal show={showPopup} onHide={handleClose}>
+    <Modal.Header closeButton>
+      <Modal.Title>{metadata.title}</Modal.Title>
+    </Modal.Header>
+    <Modal.Body>
+      <Link to={{ pathname: '/player', search: `?url=${metadata.video}` }}>
+        <img className="img-fluid" src={metadata.poster} alt={""} />
+      </Link>
+      <div className="mt-3">
+        <Table>
+          <tbody>
+            <tr>
+              <td><b>Release:</b></td>
+              <td>{metadata.date}</td>
+            </tr>
+            <tr>
+              <td><b>Genre:</b></td>
+              <td>
+                {metadata.genre.map((genre, index) => (
+                  <Badge key={index} bg="primary" className="me-1">{genre}</Badge>
+                ))}
+              </td>
+            </tr>
+            <tr>
+              <td><b>Length:</b></td>
+              <td>??</td>
+            </tr>
+            <tr>
+              <td><b>Description:</b></td>
+              <td>{metadata.description}</td>
+            </tr>
+          </tbody>
+        </Table>
       </div>
-      
-    </span>
-  );
+    </Modal.Body>
+    <Modal.Footer>
+      <Button variant="secondary" onClick={handleClose}>
+        Close
+      </Button>
+    </Modal.Footer>
+  </Modal>
+);
+
+
 }
 
 export default Popup;
